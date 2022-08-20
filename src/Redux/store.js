19f3805371bcd6077733475386f07d7e8a3f355c
malik-zee-code <ze3063786@gmail.com>
2022-08-21 01:45:49 +0500

    Added cart

--- src/Redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import UserReducer from "./User/UserSlice";
-import FaqReducer from "./Faq/FaqSlice";
-
-const store = configureStore({
-  reducer: { User: UserReducer, Faq: FaqReducer },
-});
-
-export default store;
